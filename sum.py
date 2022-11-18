183d77fd455a51743f86eaa01c963c462a723405
hvj1234 <harsath12345@gmail.com>
2022-11-17 21:41:18 -0900

    added subtract

--- sum.py
+++ sum.py
@@ -3,4 +3,10 @@ def sum():
     b=int(input("Enter b value "))
     sum=a+b
     print(sum)
+def sub():
+    a=int(input("Enter a value "))
+    b=int(input("Enter b value "))
+    sub=a-b
+    print(sub)
 sum()
+sub()
