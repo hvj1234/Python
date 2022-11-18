b42fb31a7d125d9fc25bea6794add6a34ab1a4f7
hvj1234 <harsath12345@gmail.com>
2022-11-17 21:53:26 -0900

    area of triangle

---
+++ area.py
@@ -0,0 +1,6 @@
+def triangle(h,b):
+    a=0.5*h*b
+    print(f"{a} is the area of triangle")
+a=int(input("Enter the height of triangle ")) 
+b=int(input("Enter the breadth of triangle ")) 
+triangle(a,b)
